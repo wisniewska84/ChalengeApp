3054c30966f4195cda167d4f1924eeae75ed37dd
Anna Król <wisniewska@poczta.onet.pl>
2024-01-21 17:42:33 +0100

    zadanie dzień numer 4

--- ChalengeApp/ChalengeApp/Program.cs
+++ ChalengeApp/ChalengeApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Zadanie domowe dzień 4
+
+
+var name = "Ewa";
+var age = 18;
+var sex = "Female";
+
+
+if (sex == "Female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (sex == "male" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("");
+}
